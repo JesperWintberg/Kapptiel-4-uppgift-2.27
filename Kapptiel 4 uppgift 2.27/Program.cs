093c437aa81477b270aeb227caba5276f5d055fa
TheBlueSoul5188 <hibba04@hotmail.se>
2022-10-21 10:29:09 +0200

    Add project files.

---
+++ Kapptiel 4 uppgift 2.27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ditt medelande");
+            string medelande = Console.ReadLine();
+            Console.WriteLine("Hur långt åt vänster vill du flytta medelandet.");
+            string steg = Console.ReadLine();
+
+            int flyttning = int.Parse(steg);
+
+            for (int i=0; i<medelande.Length; i++)
+            {
+                for (int j = 0; j < flyttning; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(medelande[i]);
+            }  
+
+        }
+    }
+}
